d752966060b0ca3c7893315555b786481cae1cc2
Luigi Cerone <luigicerone96@gmail.com>
2019-07-12 11:35:06 +0200

    [fix] minor changes

--- Examples/advanced/work/agent3.pl
+++ Examples/advanced/work/agent3.pl
@@ -13,35 +13,39 @@
 
 :-dynamic replyRight/1.
 
-eve(init(var_Id)):-print('Agent init->'),print('Id:'),print(var_Id),nl,assert(myid(var_Id)),var_L is(var_Id-1)mod 4,assert(left(var_L)),var_R is(var_Id+1)mod 4,assert(right(var_R)),start(var_Id).
+eve(init(var_Id)):-print('Agent init->'),print('Id:'),print(var_Id),nl,assert(myid(var_Id)),var_L is(var_Id-1)mod 4,assert(left(var_L)),var_R is(var_Id+1)mod 4,assert(right(var_R)),print('dx:'),print(var_R),print(' sx:'),print(var_L),nl,start(var_Id).
 
 start(var_Id):-right(var_RightAgent),var_X=agent,getAgentName(var_X,var_RightAgent,var_Result),format(user_output,'election(~w,~w,~w,~w)',[var_Id,var_Id,0,1]),nl,a(message(var_Result,send_message(election(var_Id,var_Id,0,1),var_Me))).
 
 getAgentName(var_AtomicPrefix,var_NumericSuffix,var_Concatenation):-name(var_AtomicPrefix,var_Pfx),name(var_NumericSuffix,var_Sfx),append(var_Pfx,var_Sfx,var_Codes),name(var_Concatenation,var_Codes).
 
-eve(election(var_Id,var_SenderId,var_Phase,var_Distance)):-print(election_chiamato),nl,myid(var_MyId),print(ho_ricevuto_election_da_),print(var_Id),nl,(var_Id>var_MyId->var_FDistance is var_Distance/1,(var_FDistance=:=2**var_Phase->print(fdistance_uguale_duephase),nl,sendReply(var_Id,var_SenderId,var_Phase,var_Distance);var_FDistance<2**var_Phase->print(fdistance_minore_duephase),nl,var_NewDist is var_Distance+1,forwardElection(var_Id,var_MyId,var_Phase,var_NewDist);print(post_instance),var_FDistance>2**var_Phase->print(errore_fd));var_Id==var_MyId->announce(var_MyId);var_Id<var_MyId->print(non_inoltro),nl).
+eve(election(var_Id,var_SenderId,var_Phase,var_Distance)):-handleEvent(var_Id,var_SenderId,var_Phase,var_Distance).
+
+handleEvent(var_Id,var_SenderId,var_Phase,var_Distance):-print(election_chiamato_from_),print(var_SenderId),nl,myid(var_MyId),(var_Id>var_MyId->var_FDistance is var_Distance/1,(var_FDistance=:=2**var_Phase->print(fdistance_uguale_duephase),nl,sendReply(var_Id,var_SenderId,var_Phase,var_Distance);var_FDistance<2**var_Phase->print(fdistance_minore_duephase),nl,var_NewDist is var_Distance+1,forwardElection(var_Id,var_MyId,var_Phase,var_NewDist);print(post_instance),var_FDistance>2**var_Phase->print(errore_fd));var_Id==var_MyId->announce(var_MyId);var_Id<var_MyId->print(non_inoltro),nl,nl).
 
 announce(var_Id):-a(message(agent1,send_message(leader(var_Id),var_Me))).
 
 forwardElection(var_Id,var_SenderId,var_Phase,var_NewDist):-left(var_LeftAgent),right(var_RightAgent),(var_Id==var_LeftAgent->sendElectionToAgent(var_RightAgent,var_Id,var_Phase,var_NewDist);var_Id==var_RightAgent->sendElectionToAgent(var_LeftAgent,var_Id,var_Phase,var_NewDist)).
 
-sendElectionToAgent(var_AgentCode,var_Id,var_Phase,var_NewDist):-var_X=agent,getAgentName(var_X,var_AgentCode,var_Result),myid(var_MyId),a(message(var_Result,send_message(election(var_Id,var_MyId,var_Phase,var_NewDist),var_Me))).
+sendElectionToAgent(var_AgentCode,var_Id,var_Phase,var_NewDist):-var_X=agent,getAgentName(var_X,var_AgentCode,var_Result),myid(var_MyId),print(forward_election_from_),print(var_MyId),print(to),print(var_AgentCode),nl,print(forward_content_is_id_),print(var_Id),print(phase_),print(var_Phase),print(dist),print(var_NewDist),nl,a(message(var_Result,send_message(election(var_Id,var_MyId,var_Phase,var_NewDist),var_Me))).
 
-sendReply(var_Id,var_SenderId,var_Phase,var_Distance):-print(send_reeply_from_user_),print(var_SenderId),nl,left(var_LeftAgent),right(var_RightAgent),(var_SenderId==var_LeftAgent->sendReplyToAgent(var_LeftAgent,var_Id,var_Phase,var_Distance);var_SenderId==var_RightAgent->sendReplyToAgent(var_RightAgent,var_Id,var_Phase,var_Distance)).
+sendReply(var_Id,var_SenderId,var_Phase,var_Distance):-myid(var_MyId),print(send_reply_from_user_),print(var_MyId),nl,left(var_LeftAgent),right(var_RightAgent),(var_SenderId==var_LeftAgent->sendReplyToAgent(var_LeftAgent,var_Id,var_Phase,var_Distance);var_SenderId==var_RightAgent->sendReplyToAgent(var_RightAgent,var_Id,var_Phase,var_Distance)).
 
-sendReplyToAgent(var_AgentCode,var_Id,var_Phase,var_Distance):-print(send_reply_to_agent_),print(var_AgentCode),nl,var_X=agent,getAgentName(var_X,var_AgentCode,var_Result),myid(var_MyId),a(message(var_Result,send_message(reply(var_Id,var_MyId,var_Phase,var_Distance),var_Me))).
+sendReplyToAgent(var_AgentCode,var_Id,var_Phase,var_Distance):-print(send_reply_to_agent_),print(var_AgentCode),nl,nl,var_X=agent,getAgentName(var_X,var_AgentCode,var_Result),myid(var_MyId),a(message(var_Result,send_message(reply(var_Id,var_MyId,var_Phase,var_Distance),var_Me))).
 
-eve(reply(var_Id,var_SenderId,var_Phase,var_Distance)):-myid(var_MyId),left(var_LeftAgent),right(var_RightAgent),print(ho_ricevuto_reply_da_),print(var_SenderId),nl,(var_Id==var_MyId->(var_LeftAgent==var_SenderId->replyFromLeft(var_Id,var_Phase,var_Distance);var_RightAgent==var_SenderId->replyFromRight(var_Id,var_Phase,var_Distance));forwardReply(var_Id,var_SenderId,var_Phase)).
+eve(reply(var_Id,var_SenderId,var_Phase,var_Distance)):-myid(var_MyId),left(var_LeftAgent),right(var_RightAgent),print(ho_ricevuto_reply_da_),print(var_SenderId),nl,(var_Id==var_MyId->(var_LeftAgent==var_SenderId->replyFromLeft(var_Id,var_Phase,var_Distance);var_RightAgent==var_SenderId->replyFromRight(var_Id,var_Phase,var_Distance));forwardReply(var_Id,var_SenderId,var_Phase,var_Distance)).
 
 replyFromLeft(var_Id,var_Phase,var_Distance):-left(var_LeftAgent),assert(replyLeft(var_LeftAgent)),right(var_RightAgent),replyRight(var_ReplyRight),print(vado_nella_next),nl,goNextPhase(var_Phase).
 
-replyFromRight(var_Id,var_Phase,var_Distance):-right(var_RightAgent),print(sto_eseguendo_reply_from_right),print(var_RightAgent),nl,assert(replyRight(var_RightAgent)),print(sono_qui),nl,left(var_LeftAgent),var_X=agent,getAgentName(var_X,var_LeftAgent,var_Result),print(torno_dietro_con_distance_),print(var_Distance),print(e_phase_),print(var_Phase),nl,a(message(var_Result,send_message(election(var_Id,var_Id,var_Phase,var_Distance),var_Me))).
+replyFromRight(var_Id,var_Phase,var_Distance):-right(var_RightAgent),print(sto_eseguendo_reply_from_right),print(var_RightAgent),nl,assert(replyRight(var_RightAgent)),left(var_LeftAgent),var_X=agent,getAgentName(var_X,var_LeftAgent,var_Result),myid(var_MyId),print(torno_dietro_invio_handle_event_from),print(var_MyId),print(to),print(var_Result),print(with_val_id_),print(var_Id),print(distance_),print(var_Distance),print(and_phase_),print(var_Phase),nl,nl,a(message(var_Result,execute_proc(handleEvent(var_Id,var_Id,var_Phase,1),var_Me))).
 
-forwardReply(var_Id,var_SenderId,var_Phase):-left(var_LeftAgent),right(var_RightAgent),(var_SenderId==var_LeftAgent->sendReplyToAgent(var_RightAgent,var_Id,var_Phase);var_SenderId==var_RightAgent->sendReplyToAgent(var_LeftAgent,var_Id,var_Phase)).
+forwardReply(var_Id,var_SenderId,var_Phase,var_Distance):-left(var_LeftAgent),right(var_RightAgent),(var_SenderId==var_LeftAgent->sendReplyToAgent(var_RightAgent,var_Id,var_Phase,var_Distance);var_SenderId==var_RightAgent->sendReplyToAgent(var_LeftAgent,var_Id,var_Phase,var_Distance)).
 
-goNextPhase(var_Phase):-print(sono_go_next_phase),nl,var_NextPhase is var_Phase+1,right(var_RightAgent),myid(var_MyId),var_X=agent,getAgentName(var_X,var_RightAgent,var_Result),print(mando_a_),print(var_Result),nl,myid(var_MioId),print(io_sono_),print(var_MioId),nl,var_X=agent,getAgentName(var_X,var_MioId,var_This),var_P is var_MioId+0,integer(var_P),print(se_stampo_p_is_intero),nl,integer(var_MioId),print(se_stampo_myid_is_intero),nl,a(message(var_Result,send_message(test(var_MioId),var_This))),a(message(var_Result,send_message(election(3,3,var_NextPhase,1),var_This))),print(ho_inviato).
+goNextPhase(var_Phase):-print(sono_go_next_phase),nl,var_NextPhase is var_Phase+1,right(var_RightAgent),myid(var_MyId),var_X=agent,getAgentName(var_X,var_RightAgent,var_Result),print(mando_a_),print(var_Result),nl,myid(var_MioId),print(io_sono_),print(var_MioId),nl,a(message(var_Result,execute_proc(handleEvent(var_MioId,var_MioId,var_NextPhase,1),var_Me))),print(ho_inviato).
 
 eve(test(var_Id)):-print(ci_sono),print(var_Id),nl,integer(var_Id),print(se_stampo_allora_id_is_intero),nl.
+
+t(var_Id):-print(sasa_),print(var_Id),nl.
 
 :-dynamic receive/1.
 
@@ -133,11 +137,11 @@ call_inform(var_X,var_Ag,var_T):-asse_cosa(past_event(inform(var_X,var_Ag),var_T
 
 call_refuse(var_X,var_Ag,var_T):-clause(agent(var_A),var__),asse_cosa(past_event(var_X,var_T)),statistics(walltime,[var_Tp,var__]),retractall(past(var_X,var__,var_Ag)),assert(past(var_X,var_Tp,var_Ag)),a(message(var_Ag,reply(received(var_X),var_A))).
 
-call_cfp(var_A,var_C,var_Ag):-clause(agent(var_AgI),var__),clause(ext_agent(var_Ag,_462113,var_Ontology,_462117),_462107),asserisci_ontologia(var_Ag,var_Ontology,var_A),once(call_meta_execute_cfp(var_A,var_C,var_Ag,_462151)),a(message(var_Ag,propose(var_A,[_462151],var_AgI))),retractall(ext_agent(var_Ag,_462189,var_Ontology,_462193)).
+call_cfp(var_A,var_C,var_Ag):-clause(agent(var_AgI),var__),clause(ext_agent(var_Ag,_475667,var_Ontology,_475671),_475661),asserisci_ontologia(var_Ag,var_Ontology,var_A),once(call_meta_execute_cfp(var_A,var_C,var_Ag,_475705)),a(message(var_Ag,propose(var_A,[_475705],var_AgI))),retractall(ext_agent(var_Ag,_475743,var_Ontology,_475747)).
 
-call_propose(var_A,var_C,var_Ag):-clause(agent(var_AgI),var__),clause(ext_agent(var_Ag,_461987,var_Ontology,_461991),_461981),asserisci_ontologia(var_Ag,var_Ontology,var_A),once(call_meta_execute_propose(var_A,var_C,var_Ag)),a(message(var_Ag,accept_proposal(var_A,[],var_AgI))),retractall(ext_agent(var_Ag,_462057,var_Ontology,_462061)).
+call_propose(var_A,var_C,var_Ag):-clause(agent(var_AgI),var__),clause(ext_agent(var_Ag,_475541,var_Ontology,_475545),_475535),asserisci_ontologia(var_Ag,var_Ontology,var_A),once(call_meta_execute_propose(var_A,var_C,var_Ag)),a(message(var_Ag,accept_proposal(var_A,[],var_AgI))),retractall(ext_agent(var_Ag,_475611,var_Ontology,_475615)).
 
-call_propose(var_A,var_C,var_Ag):-clause(agent(var_AgI),var__),clause(ext_agent(var_Ag,_461875,var_Ontology,_461879),_461869),not(call_meta_execute_propose(var_A,var_C,var_Ag)),a(message(var_Ag,reject_proposal(var_A,[],var_AgI))),retractall(ext_agent(var_Ag,_461931,var_Ontology,_461935)).
+call_propose(var_A,var_C,var_Ag):-clause(agent(var_AgI),var__),clause(ext_agent(var_Ag,_475429,var_Ontology,_475433),_475423),not(call_meta_execute_propose(var_A,var_C,var_Ag)),a(message(var_Ag,reject_proposal(var_A,[],var_AgI))),retractall(ext_agent(var_Ag,_475485,var_Ontology,_475489)).
 
 call_accept_proposal(var_A,var_Mp,var_Ag,var_T):-asse_cosa(past_event(accepted_proposal(var_A,var_Mp,var_Ag),var_T)),statistics(walltime,[var_Tp,var__]),retractall(past(accepted_proposal(var_A,var_Mp,var_Ag),var__,var_Ag)),assert(past(accepted_proposal(var_A,var_Mp,var_Ag),var_Tp,var_Ag)).
 
@@ -145,7 +149,7 @@ call_reject_proposal(var_A,var_Mp,var_Ag,var_T):-asse_cosa(past_event(rejected_p
 
 call_failure(var_A,var_M,var_Ag,var_T):-asse_cosa(past_event(failed_action(var_A,var_M,var_Ag),var_T)),statistics(walltime,[var_Tp,var__]),retractall(past(failed_action(var_A,var_M,var_Ag),var__,var_Ag)),assert(past(failed_action(var_A,var_M,var_Ag),var_Tp,var_Ag)).
 
-call_cancel(var_A,var_Ag):-if(clause(high_action(var_A,var_Te,var_Ag),_461439),retractall(high_action(var_A,var_Te,var_Ag)),true),if(clause(normal_action(var_A,var_Te,var_Ag),_461473),retractall(normal_action(var_A,var_Te,var_Ag)),true).
+call_cancel(var_A,var_Ag):-if(clause(high_action(var_A,var_Te,var_Ag),_474993),retractall(high_action(var_A,var_Te,var_Ag)),true),if(clause(normal_action(var_A,var_Te,var_Ag),_475027),retractall(normal_action(var_A,var_Te,var_Ag)),true).
 
 external_refused_action_propose(var_A,var_Ag):-clause(not_executable_action_propose(var_A,var_Ag),var__).
 
@@ -153,17 +157,17 @@ evi(external_refused_action_propose(var_A,var_Ag)):-clause(agent(var_Ai),var__),
 
 refused_message(var_AgM,var_Con):-clause(eliminated_message(var_AgM,var__,var__,var_Con,var__),var__).
 
-refused_message(var_To,var_M):-clause(eliminated_message(var_M,var_To,motivation(conditions_not_verified)),_461255).
+refused_message(var_To,var_M):-clause(eliminated_message(var_M,var_To,motivation(conditions_not_verified)),_474809).
 
 evi(refused_message(var_AgM,var_Con)):-clause(agent(var_Ai),var__),a(message(var_AgM,inform(var_Con,motivation(refused_message),var_Ai))),retractall(eliminated_message(var_AgM,var__,var__,var_Con,var__)),retractall(eliminated_message(var_Con,var_AgM,motivation(conditions_not_verified))).
 
-send_jasper_return_message(var_X,var_S,var_T,var_S0):-clause(agent(var_Ag),_461103),a(message(var_S,send_message(sent_rmi(var_X,var_T,var_S0),var_Ag))).
+send_jasper_return_message(var_X,var_S,var_T,var_S0):-clause(agent(var_Ag),_474657),a(message(var_S,send_message(sent_rmi(var_X,var_T,var_S0),var_Ag))).
 
-gest_learn(var_H):-clause(past(learn(var_H),var_T,var_U),_461051),learn_if(var_H,var_T,var_U).
+gest_learn(var_H):-clause(past(learn(var_H),var_T,var_U),_474605),learn_if(var_H,var_T,var_U).
 
-evi(gest_learn(var_H)):-retractall(past(learn(var_H),_460927,_460929)),clause(agente(_460949,_460951,_460953,var_S),_460945),name(var_S,var_N),append(var_L,[46,112,108],var_N),name(var_F,var_L),manage_lg(var_H,var_F),a(learned(var_H)).
+evi(gest_learn(var_H)):-retractall(past(learn(var_H),_474481,_474483)),clause(agente(_474503,_474505,_474507,var_S),_474499),name(var_S,var_N),append(var_L,[46,112,108],var_N),name(var_F,var_L),manage_lg(var_H,var_F),a(learned(var_H)).
 
-cllearn:-clause(agente(_460721,_460723,_460725,var_S),_460717),name(var_S,var_N),append(var_L,[46,112,108],var_N),append(var_L,[46,116,120,116],var_To),name(var_FI,var_To),open(var_FI,read,_460821,[]),repeat,read(_460821,var_T),arg(1,var_T,var_H),write(var_H),nl,var_T==end_of_file,!,close(_460821).
+cllearn:-clause(agente(_474275,_474277,_474279,var_S),_474271),name(var_S,var_N),append(var_L,[46,112,108],var_N),append(var_L,[46,116,120,116],var_To),name(var_FI,var_To),open(var_FI,read,_474375,[]),repeat,read(_474375,var_T),arg(1,var_T,var_H),write(var_H),nl,var_T==end_of_file,!,close(_474375).
 
 send_msg_learn(var_T,var_A,var_Ag):-a(message(var_Ag,confirm(learn(var_T),var_A))).
 
